d83de85f69489745cf1679469d28d39a58293305
Adam Breaux <breauxaj@gmail.com>
2015-10-28 19:36:48 -0400

    wrong name

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,12 +33,12 @@ class phing (
     path    => '/bin:/usr/bin',
     command => "wget ${source}",
     cwd     => '/usr/local/bin',
-    creates => '/usr/local/bin/phing.phar',
+    creates => '/usr/local/bin/phing-latest.phar',
     timeout => 10000,
-    onlyif  => "test ! -f /usr/local/bin/phing.phar",
+    onlyif  => "test ! -f /usr/local/bin/phing-latest.phar",
   }
 
-  file { '/usr/local/bin/phing.phar':
+  file { '/usr/local/bin/phing-latest.phar':
     ensure  => present,
     owner   => 'root',
     group   => 'root',
@@ -50,7 +50,7 @@ class phing (
     ensure  => 'link',
     owner   => 'root',
     group   => 'root',
-    target  => "/usr/local/bin/phing.phar",
+    target  => "/usr/local/bin/phing-latest.phar",
     require => Exec['get-phing'],
   }
 
